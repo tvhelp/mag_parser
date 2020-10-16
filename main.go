54e0eef3b92870a94e8e22fa1f5972ef276c7f48
Andrey Kruglov <kruglov.it@gmail.com>
2020-10-16 16:49:20 +0300

    Update

--- main.go
+++ main.go
@@ -6,18 +6,32 @@ import (
 )
 
 const defaultName = "MAG5.1-160-236(740.S.48R.AS)"
-const industryName = "INDUSTRY.3-160-236(740.G.BR.AS)"
+const industryName = "INDUSTRY.3-160-236(740.G.BR2.AS)"
+const ballName = "BALL400-060(840.N)"
+const ledparkName = "LedPark02-028"
 
-var magRgx = regexp.MustCompile(`(([A-Z]{3,}\.?[\d\.]+)-(\d+)-(\d)(\d+))\w?\((\d)(\d+)\.([A-Z])\.(\d*[A-Z]+\d*)\.([A-Z]+)\)`)
+//var magRgx = regexp.MustCompile(`(([A-Z]{3,}\.?\w?[\d\.]+)-(\d+)-(\d)(\d+))\w?\((\d)(\d+)\.([A-Z])\.(\d*[A-Z]+\d*)\.([A-Z]+)\)`)
+
+var magRgx = regexp.MustCompile(`(MAG|INDUSTRY|BALL|LedPark)\.?[\d\.]*-\d+(-\d{3})?(\w?\(\d{3}(\.[A-Z])?(\.\d*[A-Z]+\d*)?(\.[A-Z]+)?\))?`)
 
 func main() {
 	if magRgx.MatchString(defaultName) {
-		submatches := magRgx.FindAllStringSubmatch(defaultName, -1)[0][1:]
-		fmt.Println(submatches)
+		// submatches := magRgx.FindAllStringSubmatch(defaultName, -1)[0][1:]
+		// fmt.Println(submatches)
+		fmt.Println("MAG completed")
 	}
 
 	if magRgx.MatchString(industryName) {
-		submatches := magRgx.FindAllStringSubmatch(industryName, -1)[0][1:]
-		fmt.Println(submatches)
+		// submatches := magRgx.FindAllStringSubmatch(industryName, -1)[0][1:]
+		// fmt.Println(submatches)
+		fmt.Println("MAG completed")
+	}
+
+	if magRgx.MatchString(ballName) {
+		fmt.Println("BALL completed")
+	}
+
+	if magRgx.MatchString(ledparkName) {
+		fmt.Println("LedPark completed")
 	}
 }
---
+++ model_parameters.go
@@ -0,0 +1,21 @@
+package main
+
+// var params [][]string = [
+//     // Old params
+//     [ "series", "([A-Z]{3,}\.?[0-9A-Z\.]+)" ],
+//     [ "power", "(\d+)" ],
+//     [ "clusterNumber", "(\d)" ],
+//     [ "ledNumberPerCluster", "(\d+)" ],
+//     [ "colorRenderingIndex", "(\d)" ],
+//     [ "colorTemperature", "(\d+)" ],
+//     [ "lightIntensityCurveType": "([A-Z])" ],
+//     [ "mountType", "(\d*[A-Z]+\d*)" ],
+//     [ "lightFlowControl", "([A-Z]+)" ]
+
+// 	// New params
+// 	[ "dimensions", "(H\d+V\d+)|(L\d+)|(D\d+/\d+)" ],
+// 	[ "lightIntensityCurveType": "([A-Z0-9]+)" ],
+// 	[ "mountType", "(\d*[A-Z/]+\d*)" ],
+// 	[ "lightColorParams": "([A-Z0-9]{3,}"]
+// 	[ "led": (\d)]
+// ]
